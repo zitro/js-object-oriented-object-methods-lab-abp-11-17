4423a6222facf6614fca3cea952c44bbeb62cb57
Bryan Ortiz <bryan@bryanortiz.com>
2017-11-09 23:15:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function BoardMember(name, homeState, training){
     return 'You can do that!';
   }
   this.doCharity = function(){
-    return 'I like to help people';
+    return 'I like to help people.';
   }
   this.releasePressStatement = function(){
     return 'You will see great things from Scuber.';
